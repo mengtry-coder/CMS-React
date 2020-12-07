38f22700d49ba4ebd5da4edcbe241d7c4c9a6c81
Mengtry <measmengtry191@gmail.com>
2020-12-07 23:17:41 +0700

    frontend cover image

--- src/components/layouts/frontend.js
+++ src/components/layouts/frontend.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import "../../frontend_style.css";
 import Logo from "./../../images/Cms-logo-sample-1.svg";
-import { Row, Col, Image } from "antd";
+import { Row, Col, Image, Button, Divider, Carousel } from "antd";
 import {
-    MenuFoldOutlined
+    MenuFoldOutlined,
+    RightCircleOutlined
   } from "@ant-design/icons";
 
 function frontend(props) {
@@ -11,11 +12,8 @@ function frontend(props) {
         <div>
             <div className="top-menu">
                 <Row>
-                    <Col span={12}>
-                        <Image
-                            width={40}
-                            src={Logo} 
-                        />
+                    <Col className="menu-logo" span={12}>
+                        CMS React
                     </Col>
                     <Col className="menu-title" span={12}>
                         <MenuFoldOutlined /> MENU
@@ -23,7 +21,27 @@ function frontend(props) {
                 </Row>
             </div>
             <div className="cover-image">
-                <Image src="https://wallup.net/wp-content/uploads/2017/11/17/230769-luxury_watches-watch.jpg" />
+                <Row>
+                    <Col className="right-corner" span={5}>
+                        
+                    </Col>
+                    <Col className="menu-title" span={19}>
+                        <Carousel autoplay>
+                            <div className="slider">
+                                <Image src="https://wallup.net/wp-content/uploads/2017/11/17/230769-luxury_watches-watch.jpg" />
+                            </div>
+                            <div className="slider">
+                                <Image src="https://free4kwallpapers.com/uploads/originals/2020/05/12/clock-mechanism-by-frank-pfeiffer-wallpaper.jpg" />
+                            </div>
+                        </Carousel>,
+                    </Col>
+                    <div className="cover-text">
+                        <p>________ Brands</p>
+                        <p className="brand_title">Web Technology Cycle</p>
+                        <p>Cal was first. The first public university in the great state of California. They are the pioneers. They are the trailblazers who started it all.</p>
+                        <br></br><br></br><a className="view_more" href=""><RightCircleOutlined />   View More</a>
+                    </div>
+                </Row>
             </div>
               {props.children}
             Footer Here
